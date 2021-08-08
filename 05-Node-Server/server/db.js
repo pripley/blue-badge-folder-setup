03120dcd45e4cd373519acb041de529b57f4c926
Paul Ripley <paulripley@hotmail.com>
2021-08-08 15:52:50 -0400

    Journal Server / DB file formatting updated

--- 05-Node-Server/server/db.js
+++ 05-Node-Server/server/db.js
@@ -1,18 +1,18 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 // Option 1: Passing parameters separately
-const sequelize = new Sequelize('journal-walkthrough', 'postgres', 'password', {
-  host: 'localhost',
-  dialect: 'postgres'
+const sequelize = new Sequelize("journal-walkthrough", "postgres", "password", {
+  host: "localhost",
+  dialect: "postgres",
 });
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
   });
 
-  module.exports = sequelize
+module.exports = sequelize;
